96f8faf1b6eb0d0b8c5e5a2c31cd90ff7cb148e6
ganxinming <@ganBadribbit127>
2021-08-26 10:01:15 +0800

    feat: 测试git2

--- ourspring/springMvc/src/main/java/com/ourGut/ourSpring/springmvc/springbootApp.java
+++ ourspring/springMvc/src/main/java/com/ourGut/ourSpring/springmvc/springbootApp.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class springbootApp {
 	public static void main(String[] args) {
+		System.out.println("测试git2");
 		SpringApplication.run(springbootApp.class,args);
 	}
 }
